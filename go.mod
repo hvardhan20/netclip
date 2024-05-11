4b2b44f0291ad84571354e78d9f6354d3e03d8a5
Harsha <hvardhan20@gmail.com>
2024-05-11 02:57:29 -0500

    Added file feat

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module netclip
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 type Content struct {
 	Data string
+}
+
+type File struct {
+	Name string
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -73,15 +82,74 @@ func checkErr(err error) {
 	}
 }
 
+func files(w http.ResponseWriter, r *http.Request) {
+
+	entries, err := os.ReadDir("tmp")
+	checkErr(err)
+	w.Header().Set("Content-Type", "application/json")
+	var files []File
+	for _, e := range entries {
+		// fmt.Println(e.Name())
+		d := File{e.Name()}
+		files = append(files, d)
+	}
+	json.NewEncoder(w).Encode(files)
+
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploading %v", handler.Filename)
+	if _, err := os.Stat("tmp"); os.IsNotExist(err) {
+		os.MkdirAll("tmp", 0777) // Create your file
+	}
+	tempFile, err := os.Create(fmt.Sprintf("tmp/%s", handler.Filename))
+	checkErr(err)
+	defer tempFile.Close()
+	fileBytes, err := ioutil.ReadAll(file)
+	checkErr(err)
+	tempFile.Write(fileBytes)
+	fmt.Fprintf(w, "Uploaded file!")
+
+}
+
+func getFile(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fileName, ok := vars["filename"]
+	if !ok {
+		fmt.Println("file is missing in parameters")
+	}
+	fmt.Printf("file is %v", fileName)
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s", fileName))
+	w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", r.Header.Get("Content-Length"))
+	body, err := os.ReadFile(fmt.Sprintf("tmp/%s", fileName))
+
+	checkErr(err)
+	io.Copy(w, strings.NewReader(string(body)))
+}
+
 func main() {
 	fmt.Println("Starting Netclip...")
 	//http.HandleFunc("/", home)
 
 	//fs := http.FileServer(http.Dir("./static"))
 	//http.Handle("/", fs)
-	http.HandleFunc("/", home)
-	http.HandleFunc("/save", save)
-	err := http.ListenAndServe(":8081", nil)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/save", save)
+	r.HandleFunc("/upload", upload)
+	r.HandleFunc("/file", files)
+	r.HandleFunc("/file/{filename}", getFile)
+
+	err := http.ListenAndServe(":8081", r)
 	if err != nil {
 		panic("Server failed")
 	}
